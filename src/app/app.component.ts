72eb8709caa1f1919ca84b0d8189ea4fa0d7fd85
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-02-25 17:58:28 -0800

    call out object names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ourInterfaces } from './shared/my-interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-typescript-interfaces';
+
+  ourCust: ourInterfaces.Customer;
+  ourAnimal: ourInterfaces.Animal;
+  allObjs: {any}[];
+
+  ngOnInit() {
+    const ourCust = this.ourCust = {
+      id: 1,
+      name: 'Bob',
+      age: 26
+    };
+
+    const ourAnimal = this.ourAnimal = {
+      id: 1,
+      name: 'Sir Cuddles',
+      hungerLevel: 200000000
+    };
+
+    const ourObjs = [ourCust, ourAnimal];
+    this.ourCustomRender(ourObjs);
+  }
+
+  ourCustomRender(ourObjs) {
+    this.allObjs = ourObjs;
+    console.log(this.allObjs, ' all page objs');
+  }
+
 }
---
+++ src/app/shared/my-interfaces.ts
@@ -0,0 +1,13 @@
+export namespace ourInterfaces {
+    export interface Customer {
+        id: number;
+        name: string;
+        age: number;
+    }
+
+    export interface Animal {
+        id: number;
+        name: string;
+        hungerLevel: number;
+    }
+}
